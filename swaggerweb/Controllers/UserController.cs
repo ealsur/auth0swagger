c7abdeee50708df4aeff2ad18708864c2557921b
ealsur <ealsur@hotmail.com>
2017-01-27 16:55:19 -0300

    Changing to access_token

--- swaggerweb/Controllers/UserController.cs
+++ swaggerweb/Controllers/UserController.cs
@@ -8,15 +8,9 @@ using Services;
 [Authorize]
 public class UserController : Controller
 {
-    private readonly IAuth0SwaggerSampleAPI _api;
-    public UserController(){
-        
-    }
-
-    
     public async Task<IActionResult> List()
     {
-        var auth = new Microsoft.Rest.TokenCredentials(User.Claims.First(x=>x.Type == "id_token").Value);
+	var auth = new Microsoft.Rest.TokenCredentials(User.Claims.First(x=>x.Type == "access_token").Value);
         var api = new Auth0SwaggerSampleAPI(new Uri("http://localhost:5000"), auth); 
         return View(await api.ValuesGetAsync());
     }
